70d21cd8bb77f9d3c398ae60993b01353a3a6327
Dyrudis <58176532+Dyrudis@users.noreply.github.com>
2021-02-11 16:54:11 +0100

    Implémentation de la recherche d'adversaire

--- front/js/main.js
+++ front/js/main.js
@@ -1,1 +1,35 @@
 let socket = io();
+
+let form = document.getElementById("searchForm");
+let pseudoInput = document.getElementById("pseudo");
+let cancelButton = document.getElementById("cancel");
+let foundDiv = document.getElementById("found");
+
+form.addEventListener("submit", event => {
+    event.preventDefault();
+    if (pseudoInput.value) {
+        socket.emit("search", pseudoInput.value);
+        form.style.display = "none";
+        cancelButton.style.display = "block";
+    }
+});
+
+cancelButton.addEventListener("click", event => {
+    cancelButton.style.display = "none";
+    form.style.display = "block";
+    socket.emit("cancel");
+});
+
+socket.on("found", opponentName => {
+    console.log("test");
+    cancelButton.style.display = "none";
+    document.querySelector("#found h3").innerHTML = "Adversaire : " + opponentName + " !";
+    foundDiv.style.display = "block";
+});
+
+socket.on("found2", opponentName => {
+    console.log("test");
+    cancelButton.style.display = "none";
+    document.querySelector("#found h3").innerHTML = "Adversaire : " + opponentName + " !";
+    foundDiv.style.display = "block";
+});
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { WSANOTINITIALISED } = require("constants");
 const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
@@ -9,11 +10,46 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/front/html/index.html");
 });
 
-io.on("connection", (socket) => {
-    console.log("Utilisateur connecté");
+let waitingPlayer = {
+    "id": undefined,
+    "pseudo": undefined
+}
 
-    socket.on("disconnect", ()=> {
-        console.log("Utilisateur deconnecté");
+io.on("connection", (socket) => {
+
+    // Recherche d'adversaire
+    socket.on("search", pseudo => {
+        console.log(pseudo + " (" + socket.id + ") cherche un adversaire");
+
+        // Un adversaire est déjà disponible
+        if (waitingPlayer.id) {
+            console.log(waitingPlayer.pseudo + " joue contre " + pseudo + " !");
+            socket.emit("found", waitingPlayer.pseudo);
+            socket.to(waitingPlayer.id).emit("found", pseudo);
+        }
+
+        // Pas encore d'adversaire
+        else {
+            waitingPlayer.id = socket.id;
+            waitingPlayer.pseudo = pseudo;
+        }
+    });
+
+    socket.on("cancel", () => {
+        if (waitingPlayer.id == socket.id) {
+            console.log(waitingPlayer.pseudo + " (" + socket.id + ") annule sa recherche");
+            waitingPlayer.id = undefined;
+            waitingPlayer.pseudo = undefined;
+        }
+    });
+
+    // Déconnexion
+    socket.on("disconnect", () => {
+        if (waitingPlayer.id == socket.id) {
+            console.log(waitingPlayer.pseudo + " (" + socket.id + ") s'est deconnecté");
+            waitingPlayer.id = undefined;
+            waitingPlayer.pseudo = undefined;
+        }
     });
 });
 
