feb099aaa108b924e176ff5f263de7426f2a4015
Dyrudis <58176532+Dyrudis@users.noreply.github.com>
2021-02-21 12:19:15 +0100

    Ajout des méthodes "reset" et "isFinished" dans la classe Stratego

--- back/models/stratego.js
+++ back/models/stratego.js
@@ -2,64 +2,38 @@ const Pion = require("./pion.js");
 
 class Stratego {
     constructor() {
-        // Joueur actuel :
-        //  Joueur 1 = 0
-        //  Joueur 2 = 1
+        // Joueur actuel : 0 ou 1
         this.currentPlayer = 0;
-        
-        // Tableau des pions restants des 2 joueurs :
-        //  this.pionCount[0] = joueur 1
-        //  this.pionCount[1] = joueur 2
-        this.pionCount = [
-            {
-                "Maréchal": 1,
-                "Général": 1,
-                "Colonel": 2,
-                "Commandant": 3,
-                "Capitaine": 4,
-                "Lieutenant": 4,
-                "Sergeant": 4,
-                "Démineur": 5,
-                "Eclaireur": 8,
-                "Espion": 1,
-                "Bombe": 6,
-                "Drapeau": 1
-            },
-            {
-                "Maréchal": 1,
-                "Général": 1,
-                "Colonel": 2,
-                "Commandant": 3,
-                "Capitaine": 4,
-                "Lieutenant": 4,
-                "Sergeant": 4,
-                "Démineur": 5,
-                "Eclaireur": 8,
-                "Espion": 1,
-                "Bombe": 6,
-                "Drapeau": 1
-            }
-        ]
 
-        // Tableau de 10x10 représentant la grille du jeu
-        //  Valeurs possibles du tableau :
-        //   0 = sol
-        //   undefined = lac
-        //   variable de la classe Pion
+        // Tableau de 10x10 représentant la grille du jeu :
+        // 0 (case vide), undefined (case lac) ou pion
         this.tab = Array(10).fill().map(() => Array(10).fill(0));
-        this.tab[2][4] = undefined;
-        this.tab[2][5] = undefined;
-        this.tab[3][4] = undefined;
-        this.tab[3][5] = undefined;
-        this.tab[6][4] = undefined;
-        this.tab[6][5] = undefined;
-        this.tab[7][4] = undefined;
-        this.tab[7][5] = Pion(1, 0);
+        for (let x of [2, 3, 6, 7]) {
+            for (let y of [4, 5]) {
+                this.tab[x][y] = undefined;
+            }
+        }
+
+        // Tableau des pions restants des 2 joueurs :
+        this.pionCount = Array(2).fill({
+            "Maréchal": 1,
+            "Général": 1,
+            "Colonel": 2,
+            "Commandant": 3,
+            "Capitaine": 4,
+            "Lieutenant": 4,
+            "Sergeant": 4,
+            "Démineur": 5,
+            "Eclaireur": 8,
+            "Espion": 1,
+            "Bombe": 6,
+            "Drapeau": 1,
+        });
     }
 
     set(x, y, value) {
-        if (this.tab[x][y] != undefined) {
-            console.log("Impossible de placer ou de supprimer un pion dans le lac");
+        if (this.tab[x][y] == undefined) {
+            console.log("Impossible modifier une case lac");
             return false;
         }
         this.tab[x][y] = value;
@@ -86,18 +60,49 @@ class Stratego {
         }
         //vérification unité déplacable
         if (this.tab[x1][y1].id == "Bombe" || this.tab[x1][y1].id == "Drapeau") {
-            
+
             console.log("Le drapeau ainsi que les bombes sont indéplacables.")
         }
         //vérification unité éclaireur
         if (this.tab[x1][y1].id == "Eclaireur") { // verifier que si un axe change : pas un autre, et pas de saut
             //marque page louis
         }
-        
-        
-            //move
+
+
+        //move
 
         currentplayer = (currentplayer + 1) % 2;
+    }
+
+    reset() {
+        this.currentPlayer = 0;
+
+        this.tab = Array(10).fill().map(() => Array(10).fill(0));
+        for (let x of [2, 3, 6, 7]) {
+            for (let y of [4, 5]) {
+                this.tab[x][y] = undefined;
+            }
+        }
+
+        this.pionCount = Array(2).fill({
+            "Maréchal": 1,
+            "Général": 1,
+            "Colonel": 2,
+            "Commandant": 3,
+            "Capitaine": 4,
+            "Lieutenant": 4,
+            "Sergeant": 4,
+            "Démineur": 5,
+            "Eclaireur": 8,
+            "Espion": 1,
+            "Bombe": 6,
+            "Drapeau": 1
+        });
+    }
+
+    isFinished() {
+        // Partie terminée si un joueur n'a plus de drapeau ou si un joueur ne peut plus se déplacer
+        return this.pionCount.some((pions) => pions.Drapeau == 0 || Object.values(pions).slice(0, 10).every(value => value == 0));
     }
 }
 
