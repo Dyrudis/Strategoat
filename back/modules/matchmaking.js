553862752e5f8e2e45d63a12c6d62f8a3b39d417
Dyrudis <58176532+Dyrudis@users.noreply.github.com>
2021-02-26 20:05:15 +0100

    Implémentation du système d'invitations

--- back/modules/matchmaking.js
+++ back/modules/matchmaking.js
@@ -1,51 +1,5 @@
-module.exports = (io, socket, waitingPlayer) => {
+module.exports = (io, socket) => {
 
-    // Recherche d'adversaire
-    socket.on("search", () => {
-
-        let username = socket.handshake.session.username;
-        console.log(`${username} cherche un adversaire`);
-
-        // Un adversaire est déjà disponible
-        if (waitingPlayer.id) {
-            console.log(`${waitingPlayer.username} joue contre ${username} !`);
-            socket.emit("found", waitingPlayer.username);
-            socket.to(waitingPlayer.id).emit("found", username);
-
-            waitingPlayer.id = undefined;
-            waitingPlayer.username = undefined;
-        }
-
-        // Pas encore d'adversaire
-        else {
-            waitingPlayer.id = socket.id;
-            waitingPlayer.username = username;
-        }
-
-    });
-
-    // Annulation de la recherche
-    socket.on("cancel", () => {
-
-        let username = socket.handshake.session.username;
-
-        if (waitingPlayer.username && username == waitingPlayer.username) {
-            console.log(`${username} annule sa recherche`);
-            waitingPlayer.id = undefined;
-            waitingPlayer.username = undefined;
-        }
-    });
-
-    // Déconnexion
-    socket.on("disconnect", () => {
-        
-        let username = socket.handshake.session.username;
-
-        if (waitingPlayer.username && waitingPlayer.username == username) {
-            console.log(`${username} s'est deconnecté`);
-            waitingPlayer.id = undefined;
-            waitingPlayer.username = undefined;
-        }
-    });
+    // Vide pour le moment
 
 }
--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const session = require("express-session")({
 const sharedSession = require("express-socket.io-session");
 const bodyParser = require("body-parser");
 const mysql = require("mysql");
-const matchmaking = require("./back/modules/matchmaking");
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
@@ -35,20 +34,53 @@ io.use(sharedSession(session, {
     autoSave: true
 }));
 
-
-
-// Joueur dans la file d'attente
-let waitingPlayer = {
-    id: undefined,
-    username: undefined
-};
+// Liste des joueurs connectés
+players = [];
 
 io.on("connection", (socket) => {
-    
-    matchmaking(io, socket, waitingPlayer);
 
-    
+    // Lors de la connexion, on envoie les informations de session à l'utilisateur
     socket.emit("load", socket.handshake.session.username);
+
+    // Puis on ajoute le joueur à la liste des joueurs disponibles
+    players.push({
+        id: socket.id,
+        username: socket.handshake.session.username
+    });
+
+    // Et on actualise l'affichage de tous les joueurs
+    io.emit("refresh players", players);
+
+    // Fonction qui renvoie le socket.id du joueur ayant le nom username
+    function getId(username) {
+        let player = players.find(player => player.username == username);
+        return player ? player.id : undefined;
+    }
+
+    // Envoie d'une invitation
+    socket.on("send invite", username => {
+        socket.to(getId(username)).emit("get invite", socket.handshake.session.username);
+    });
+
+    // Invitation acceptée : début de la partie
+    socket.on("start game", (player1, player2) => {
+        if (socket.id == getId(player1)) {
+            socket.emit("found", player2);
+            socket.to(getId(player2)).emit("found", player1);
+        }
+        else {
+            socket.to(getId(player1)).emit("found", player2);
+            socket.emit("found", player1);
+        }
+    });
+
+    // Déconnexion
+    socket.on("disconnect", () => {
+        // Lors de la déconnexion, on supprime le joueur de la liste des joueurs disponibles
+        players = players.filter(player => player.id != socket.id);
+        // Puis on actualise l'affichage de tous les autres joueurs
+        socket.broadcast.emit("refresh players", players);
+    });
 
 });
 
@@ -65,22 +97,27 @@ app.get("/", (req, res) => {
 
 // Connexion à un compte existant
 app.post("/login", (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    if (username && password) {
-        connection.query("SELECT * FROM accounts WHERE username = ? AND password = ?", [username, password], (err, result) => {
-            if (err) throw err;
-            if (result.length > 0) {
-                req.session.username = username;
-                res.redirect("/");
-            }
-            else {
-                res.send("Nom d'utilisateur ou un mot de passe incorrect.");
-            }
-        });
+    if (req.session.username == undefined) {
+        const username = req.body.username;
+        const password = req.body.password;
+        if (username && password) {
+            connection.query("SELECT * FROM accounts WHERE username = ? AND password = ?", [username, password], (err, result) => {
+                if (err) throw err;
+                if (result.length > 0) {
+                    req.session.username = username;
+                    res.redirect("/");
+                }
+                else {
+                    res.send("Nom d'utilisateur ou un mot de passe incorrect.");
+                }
+            });
+        }
+        else {
+            res.send("Merci de renseigner un nom d'utilisateur ou un mot de passe.");
+        }
     }
     else {
-        res.send("Merci de renseigner un nom d'utilisateur ou un mot de passe.");
+        res.send("Vous êtes déjà connecté.");
     }
 });
 
