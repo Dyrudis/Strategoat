46f3ca37afc50a584f547676afdc62fdbccaaa40
Dyrudis <58176532+Dyrudis@users.noreply.github.com>
2021-03-11 11:05:01 +0100

    Ajout du drag and drop

--- front/js/game.js
+++ front/js/game.js
@@ -1,51 +1,105 @@
 // Connexion avec le serveur
 let socket = io();
-
 socket.emit("load game");
 
-document.getElementById("title").addEventListener("click", event => {
-    socket.emit("click");
-});
-
-socket.on("click", username => {
-    let p = document.createElement("p");
-    p.innerHTML = `${username} à cliqué sur la page !`;
-    p.style.backgroundColor = getRandomColor();
-    document.body.appendChild(p);
-})
-
-function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
+// Création du tableau
+for (let y = 9; y >= 0; y--) {
+    let tr = document.createElement("tr");
+    for (let x = 0; x < 10; x++) {
+        let td = document.createElement("td");
+        td.classList.add("case");
+        td.setAttribute("data-row", y);
+        td.setAttribute("data-column", x);
+        td.setAttribute("data-pion", 0);
+        td.setAttribute("ondrop", "drop(event)");
+        td.setAttribute("ondragover", "dragover(event)");
+        if ([2, 3, 6, 7].includes(x) && [4, 5].includes(y)) {
+            td.setAttribute("data-pion", undefined);
+            td.removeAttribute("ondrop");
+            td.removeAttribute("ondragover");
+        }
+        tr.appendChild(td);
     }
-    return color;
+    document.getElementById("stratego").appendChild(tr);
 }
 
-let selected = undefined;
-let pions = document.querySelectorAll(".pion");
+pionCount = {
+    "12": 1,
+    "11": 1,
+    "10": 2,
+    "9": 3,
+    "8": 4,
+    "7": 4,
+    "6": 4,
+    "5": 5,
+    "4": 8,
+    "3": 1,
+    "2": 6,
+    "1": 1,
+};
 
-pions.forEach(pion => {
-    pion.addEventListener("click", () => {
-        pions.forEach(pion => pion.classList.remove("selected"));
-        if (pion.getAttribute("data-pion") != selected) {
-            pion.classList.add("selected");
-            selected = pion.getAttribute("data-pion");
-            console.log("Selectionné : " + selected);
+function displayPionCount() {
+    for (const key in pionCount) {
+        document.querySelector("ul li[data-pion=\"" + key + "\"] span.count").innerHTML = ":" + pionCount[key];
+    }
+}
+
+displayPionCount();
+
+// Drag and Drop
+function dragstart(e) {
+    e.dataTransfer.setData("text/plain", e.target.getAttribute("data-pion") + ":" + e.target.getAttribute("data-column") + ":" + e.target.getAttribute("data-row"));
+}
+
+function dragover(e) {
+    e.preventDefault();
+}
+
+function drop(e) {
+    e.preventDefault();
+    let data = e.dataTransfer.getData("text/plain").split(":");
+    let pion = data[0], x = data[1], y = data[2];
+
+    // Déplacement d'un pion existant
+    if (x != "null" && y != "null") {
+        // previous : emplacement précédent
+        // next : nouvel emplacement
+        let previous = document.querySelector("[data-row=\"" + y + "\"][data-column=\"" + x + "\"]");
+        let next = e.target;
+
+        // Swap entre deux pions
+        if (next.getAttribute("data-pion") > 0) {
+            previous.setAttribute("data-pion", next.getAttribute("data-pion"));
+            previous.innerHTML = next.innerHTML;
+
+            next.setAttribute("data-pion", pion);
+            next.innerHTML = pion;
         }
+
+        // Déplacement vers une case vide
         else {
-            selected = undefined;
-            console.log("Deselection");
+            previous.setAttribute("data-pion", 0);
+            previous.innerHTML = "";
+            previous.removeAttribute("draggable");
+            previous.removeAttribute("ondragstart");
+
+            next.setAttribute("data-pion", pion);
+            next.innerHTML = pion;
+            next.setAttribute("draggable", true);
+            next.setAttribute("ondragstart", "dragstart(event)");
         }
-    });
-});
 
-let cases = document.querySelectorAll(".case");
+    }
 
-cases.forEach(td => {
-    td.addEventListener("click", () => {
-        let x = td.getAttribute("data-column"), y = td.getAttribute("data-row");
-        console.log(selected + " en (" + x + ", " + y + ")");
-    })
-});
+    // Ajout d'un pion sur le plateau
+    else if (e.target.getAttribute("data-pion") == 0 && pionCount[pion] > 0) {
+        e.target.setAttribute("data-pion", pion);
+        e.target.innerHTML = pion;
+        e.target.setAttribute("draggable", true);
+        e.target.setAttribute("ondragstart", "dragstart(event)");
+
+        pionCount[pion]--;
+    }
+
+    displayPionCount();
+}
